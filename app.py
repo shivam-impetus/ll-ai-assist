19192254082ddbfbab61d581a4207babeda3f532
shivam-impetus <shivam.verma@impetus.com>
2025-12-01 21:06:51 +0530

    Run FastAPI in a separate thread with Streamlit

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import streamlit as st
 from fastapi import FastAPI
+from threading import Thread
 import uvicorn
 
 api = FastAPI()
 
+
 @api.get("/api/hello")
 def hello():
+    st.write('hello called')
     return {"message": "Hello from Streamlit API!"}
 
 
+def start_api():
+    uvicorn.run("app:api", host="0.0.0.0", port=8201, reload=True)
+
+
 if __name__ == "__main__":
-    uvicorn.run("app:api", host="0.0.0.0", port=8101, reload=True)
+    Thread(target=start_api, daemon=True).start()
     st.title("Streamlit with Embedded REST API")
     st.write("API running on :8000")
