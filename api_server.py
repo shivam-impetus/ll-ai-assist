68c6e7e10ffec404d88ef99a1675a140d6011bb6
shivam-impetus <shivam.verma@impetus.com>
2025-12-01 21:30:12 +0530

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+import streamlit as st
 
 api = FastAPI()
 
 @api.get("/api/hello")
 def hello():
+    st.write('<h2>hello</h2>')
     return {"message": "Hello from Streamlit API!"}
